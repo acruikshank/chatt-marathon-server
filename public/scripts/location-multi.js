ea39458afec9aca5fd3f7ca8dfa37d76f7d0e1c3
Alex Cruikshank <alex@carbonfive.com>
2017-03-03 12:36:54 -0500

    tweaks

--- public/scripts/location-multi.js
+++ public/scripts/location-multi.js
@@ -11,43 +11,52 @@ Location = function(canvas, runners) {
   var Y_INTERCEPT = 293759.1721;
   var X_SCALE = cw / 412;
   var Y_SCALE = ch / 655;
-  var arcWidth = 2*Math.PI/ runners.length;
+  var arcWidth = 2*Math.PI * .8;/// runners.length;
   var arcCap = arcWidth - 2*Math.PI*.05;
 
   var activeIndex = 0;
+  var transition = 0;
 
   setInterval(fetchLocation, 1000);
   render();
+  // var offsets = [1,1,1,1,1,1,1,1,1,1,1,1].map(function() {return {x:-200*Math.random(), y:200*Math.random()}})
 
   function render() {
     ctx.clearRect(0,0,cw,ch);
 
     for (var index=0, runner; runner=runners[index]; index++) {
       if (runner.location && index != activeIndex) {
-        renderRunner(runner, index, 'rgba(100,100,100,.75)');
+        renderRunner(runner, index, false);
       }
     }
     if (runners[activeIndex].location)
-      renderRunner(runners[activeIndex], activeIndex, runners[activeIndex].color)
+      renderRunner(runners[activeIndex], activeIndex, runners[activeIndex].color, true)
 
     theta += dTheta;
     requestAnimationFrame(render);
   }
 
-  function renderRunner(runner, index, color) {
+  function renderRunner(runner, index, active) {
     var x = (X_SLOPE*runner.location.lon + X_INTERCEPT) * X_SCALE * .929 + 31;
     var y = (Y_SLOPE*runner.location.lat + Y_INTERCEPT) * Y_SCALE * .929 + 21;
+    // x += offsets[index].x;
+    // y += offsets[index].y;
 
-    ctx.fillStyle = ctx.strokeStyle = color;
-    ctx.beginPath();
-    ctx.arc(x,y,10,0,2*Math.PI,true);
-    ctx.fill();
+    ctx.fillStyle = 'rgba(3, 110, 156, 1)'; //active ? 'rgba(3, 110, 156, 1)' : 'rgba(100,100,100,.75)';
+    ctx.strokeStyle = 'rgba(3, 110, 156, 1)'; //active ? 'rgba(3, 110, 156, 1)' : 'rgba(100,100,100, 0.75)';
+    if(active) {
+      ctx.beginPath();
+      ctx.arc(x,y,active?10 + transition:8,0,2*Math.PI,true);
+      ctx.fill();
+    }
 
     ctx.beginPath();
-    ctx.arc(x,y,20,theta + index*arcWidth, theta+index*arcWidth+arcCap,false);
+    ctx.arc(x,y, active?(25 + transition):6,theta + index*arcWidth, theta+index*arcWidth+arcCap,false);
     ctx.lineCap = 'round';
-    ctx.lineWidth = 8;
+    ctx.lineWidth = active ? 10 : 4;
     ctx.stroke();
+
+    transition = Math.pow(transition,.9975);
   }
 
   function fetchLocation() {
@@ -56,6 +65,7 @@ Location = function(canvas, runners) {
 
   function setActive(index) {
     activeIndex = index;
+    transition = 250;
   }
 
   return {setActive:setActive}
